e48555faf5de70f42cb9f609222c3a814d77e676
EugenWagner-Git <86958671+EugenWagner-Git@users.noreply.github.com>
2021-07-13 14:01:31 +0200

    Weather and Football solved
    Könnte sein, dass der eine oder andere Fehler sich eingeschlichen hat.
    An sich ist die Logik hinter meinem Code leicht zu verstehen.
    Da ich mit GitHub nicht geübt bin, habe ich es mit Notepad gelöst, ohne Debuging. In der Theorie, müsse es passen.

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -1,5 +1,11 @@
 package de.exxcellent.challenge;
 
+import com.opencsv.CSVWriter;
+import java.io.FileWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 /**
  * The entry class for your solution. This class is only aimed as starting point and not intended as baseline for your software
  * design. Read: create your own classes and packages as appropriate.
@@ -15,11 +21,50 @@ public final class App {
     public static void main(String... args) {
 
         // Your preparation code …
-
-        String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
+		string weatherPath = "C:\Users\EWA\Documents\programming-challenge\src\main\resources\de\exxcellent\challenge\weather.csv";
+		string footballPath = "C:\Users\EWA\Documents\programming-challenge\src\main\resources\de\exxcellent\challenge\football.csv";
+		
+		
+        String dayWithSmallestTempSpread = GetWeatherDiff(weatherPath);     // Your day analysis function call …
         System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
 
-        String teamWithSmallestGoalSpread = "A good team"; // Your goal analysis function call …
+        String teamWithSmallestGoalSpread = GetFootballDiff(footballPath); // Your goal analysis function call …
         System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallestGoalSpread);
     }
+	private string GetWeatherDiff(String weatherPath){
+		int min = 500;
+		string day = "";
+		
+        try (CSVReader reader = new CSVReader(new FileReader(weatherPath))) {
+            List<String[]> w = reader.readAll();
+			
+            for(int i = 0; i<w.size(); i++){
+				int diff = Integer.parseInt(w.get(i)[1]) - Integer.parseInt(w.get(i)[2]);
+				if(min < diff){
+					min = diff;
+					day = w.get(i)[0]
+				}
+			}
+		return day;
+		}
+	}
+	private string GetFootballDiff(string footballPath){
+		int min = 500;
+		string team = "";
+		try (CSVReader reader = new CSVReader(new FileReader(footballPath))) {
+            List<String[]> f = reader.readAll();
+			
+            for(int i = 0; i<f.size(); i++){
+				int diff = Integer.parseInt(f.get(i)[5]) - Integer.parseInt(f.get(i)[6]);
+				if(diff < 0) 
+					diff = diff *(-1);
+				
+				if(diff < min){
+					min = diff;
+					team = f.get(i)[0];
+				}
+			}
+		}
+		return team;
+	}
 }
